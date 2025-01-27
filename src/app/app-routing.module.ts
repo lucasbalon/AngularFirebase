4f6eb2f9cf30cac8df0ab04ed3f02da315835eb8
Lucas Balon <lucasbalon@gmail.com>
2025-01-27 21:46:30 +0100

    Add Firebase authentication and environment setup.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthComponent }, // Route par défaut (Accueil)
+  { path: 'login', component: AuthComponent }, // Route vers "À propos"
+  //{ path: '**', component: PageNotFoundComponent } // Route pour les pages non trouvées (404)
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import {firebaseConfig} from '../environments/environment.development';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import { AuthComponent } from './components/auth/auth.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
